4677e2fd6e4aa186be0c16a24ae7fa6bd2de6185
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-05-21 19:58:37 +0300

    Add files via upload

---
+++ Scripts/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Путь
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            this.chart1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.DarkVertical;
+            this.chart1.BackImageTransparentColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Yellow;
+            this.chart1.BorderlineColor = System.Drawing.Color.DarkSalmon;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(706, 121);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(497, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 637);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Scripts/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Путь
+{
+    public partial class Form1 : Form
+    {
+        //private PictureBox pictureBox1;
+        private Timer timer;
+        MyPoint pointStart = new MyPoint(10, 15);
+        MyPoint pointEnd = new MyPoint(500f, 350f);
+        MyPoint pointCurrent;
+        float angle;
+        float distance;
+        int Step = 2;
+        List<double> distans = new List<double>();
+        float MinDistance;
+        public Form1()
+        {
+            this.Size = new Size(800, 600);
+            
+            InitializeComponent();
+            Init();
+            Start();
+            SetupTimer();
+        }
+        public void Init()
+        {
+            MinDistance = float.MaxValue;
+            distance = float.MaxValue;
+            pointCurrent = new MyPoint(pointStart.X, pointStart.Y);
+            angle = AngleInRadians();
+
+        }
+        private void Start()
+        {
+            chart1.Legends.Clear();
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series.Add(".");
+            pictureBox1.Paint += PictureBox_Paint;
+            Controls.Add(pictureBox1);
+        }
+
+        private void SetupTimer()
+        {
+            chart1.Series[1].ChartType = SeriesChartType.Point;
+            chart1.ChartAreas[0].AxisX.Title = "error rate";
+            chart1.ChartAreas[0].AxisY.Title = "Row length";
+            timer = new Timer();
+            timer.Interval = 1; // Обновление каждую секунду
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        public void Creafte()
+        {
+            for (int i =0 ; i <distans.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(distans[i], i - 1);
+                chart1.Series[1].Points.AddXY(distans[i], i - 1);
+            }
+            timer.Stop();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate(); // Перерисовать PictureBox
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            DrawWay(e.Graphics);
+        }
+        private void DrawWay(Graphics g)
+        {
+            float Radius = 10f;
+            float step = 10;
+            if (distance > Radius)
+            {
+                pointCurrent.X += step * Cos(angle);
+                pointCurrent.Y += step * Sin(angle);
+                distance = CalculateDistance(pointCurrent, pointEnd);
+                if(MinDistance>distance)
+                {
+                    MinDistance = distance;
+                }
+            }
+            else
+            {
+                NewStep();
+                return;
+            }
+            if(MinDistance<distance)
+            {
+                NewStep();
+            }
+
+            g.FillEllipse(Brushes.Black, pointStart.X, pointStart.Y, 10, 10);
+            g.FillEllipse(Brushes.Black, pointEnd.X, pointEnd.Y, 10, 10);
+            g.FillEllipse(Brushes.Black, pointCurrent.X, pointCurrent.Y, 10, 10);
+        }
+        public void NewStep()
+        {
+            distans.Add(MinDistance);
+            if (Step < 10)
+            {
+                Step++;
+                Init();
+            }
+            else
+            {
+                Creafte();
+            }
+        }
+        public float CalculateDistance(MyPoint p1, MyPoint p2)
+        {
+            float dx = p2.X - p1.X;
+            float dy = p2.Y - p1.Y;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private float AngleInRadians()
+        {
+            return (float)Arct(Math.Abs(pointStart.Y - pointEnd.Y) / Math.Abs(pointStart.X - pointEnd.X));
+        }
+        private int Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        private float Sin(float x)
+        {
+            float result = 0;
+            int sing = 1;
+            for (int i = 1; i <= Step; i++)
+            {
+                result += sing * (float)(Math.Pow(x, 2 * i - 1) / Factorial(2 * i - 1));
+                sing *= -1;
+            }
+            return result;
+        }
+        private float Arct(float x)
+        {
+            float result = 0;
+            int sing = 1;
+            for (int i = 1; i <= Step; i++)
+            {
+                result += sing * (float)(Math.Pow(x, 2 * i - 1) / (2 * i - 1));
+                sing *= -1;
+            }
+            return result;
+        }
+        private float Cos(float x)
+        {
+            float result = 0;
+            int sing = 1;
+            for (int i = 1; i <= Step; i++)
+            {
+                result += sing * (float)(Math.Pow(x, 2 * i - 2) / Factorial(2 * i - 2));
+                sing *= -1;
+            }
+            return result;
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class MyPoint
+    {
+        public float X, Y;
+        public MyPoint(float x, float y)
+        {
+            X = x; Y = y;
+        }
+    }
+}
