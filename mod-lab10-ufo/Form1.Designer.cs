cd1df1e258d6eed6b1a2458248a6fc0c00c7f716
Danil Volkov <volkov20032301@yandex.ru>
2024-05-22 12:16:46 +0300

    Init commit

---
+++ mod-lab10-ufo/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace mod_lab10_ufo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.picBoxGraph = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBoxArea = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxGraph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 815);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(171, 58);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // picBoxGraph
+            // 
+            this.picBoxGraph.Location = new System.Drawing.Point(12, 12);
+            this.picBoxGraph.Name = "picBoxGraph";
+            this.picBoxGraph.Size = new System.Drawing.Size(1217, 797);
+            this.picBoxGraph.TabIndex = 1;
+            this.picBoxGraph.TabStop = false;
+            this.picBoxGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoxGraph_Paint);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(201, 815);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(161, 58);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBoxArea
+            // 
+            this.textBoxArea.Location = new System.Drawing.Point(1129, 834);
+            this.textBoxArea.Name = "textBoxArea";
+            this.textBoxArea.Size = new System.Drawing.Size(100, 26);
+            this.textBoxArea.TabIndex = 3;
+            this.textBoxArea.Text = "10";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(963, 834);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(160, 20);
+            this.label.TabIndex = 4;
+            this.label.Text = "Окрестность точки:";
+            // 
+            // chart
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.Title = "Радиус точки";
+            chartArea1.AxisY.Title = "Члены ряда";
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(558, 26);
+            this.chart.Name = "chart";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Точность";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(658, 321);
+            this.chart.TabIndex = 5;
+            this.chart.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Точность расчетов";
+            this.chart.Titles.Add(title1);
+            this.chart.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 885);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBoxArea);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.picBoxGraph);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxGraph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox picBoxGraph;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textBoxArea;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ mod-lab10-ufo/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace mod_lab10_ufo
+{
+    public partial class Form1 : Form
+    {
+        static int x0 = 10, y0 = 10;
+        static int xk = 1400, yk = 900;
+        static double x1 = x0, y1 = y0;
+        static double distance = Math.Sqrt(Math.Pow(xk - x1, 2) + Math.Pow(yk - y1, 2));
+        static int value = 10;
+        static int step = 5;
+        static int n = 5;
+        double angle = Math.Atan((double)Math.Abs(yk - y0) / (double)Math.Abs(xk - x0));
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            picBoxGraph.Invalidate();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            create_graph();
+        }
+
+        private void create_graph()
+        {
+            chart.Series[0].Points.Clear();
+            
+            for (int n = 5; n >= 1; n--)
+            {
+                for (int value = 1; value < 100; value++)
+                {
+                    double x1 = x0, y1 = y0;
+                    double distance = Math.Sqrt(Math.Pow(xk - x1, 2) + Math.Pow(yk - y1, 2));
+
+                    int count = 0;
+                    while (distance > value && count < 1000)
+                    {
+                        x1 += step * cos(angle, n);
+                        y1 += step * sin(angle, n);
+                        distance = Math.Sqrt(Math.Pow(xk - x1, 2) + Math.Pow(yk - y1, 2));
+                        count++;
+                    }
+                    if (count < 1000)
+                    {
+                        chart.Series[0].Points.AddXY(value, n);
+                        break;
+                    }
+                }
+            }
+            chart.Visible = true;
+            chart.SaveImage("dia.png", ChartImageFormat.Png);
+
+        }
+
+        private void picBoxGraph_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.ScaleTransform(0.5f, 0.5f);
+            Brush b = new SolidBrush(Color.Indigo);
+
+            if (distance > value)
+            {
+                g.FillEllipse(b, x0 - 10, y0 - 10, 10 * 2, 10 * 2);
+                g.FillEllipse(b, xk - value, yk - value, value * 2, value * 2);
+
+                x1 += step * cos(angle, n);
+                y1 += step * sin(angle, n);
+
+                g.FillEllipse(b, (int)x1 - 5, (int)y1 - 5, 10, 10);
+
+                distance = Math.Sqrt(Math.Pow(xk - x1, 2) + Math.Pow(yk - y1, 2));
+            }
+            else
+            {
+                b = new SolidBrush(Color.Green);
+                g.FillEllipse(b, x0 - 10, y0 - 10, 10 * 2, 10 * 2);
+                g.FillEllipse(b, xk - value, yk - value, value * 2, value * 2);
+                timer.Enabled = false;
+                create_graph();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            value = int.Parse(textBoxArea.Text);
+            timer.Enabled = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double cos(double agle, int n)
+        {
+            double x = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                x += Math.Pow(-1, i - 1) * (double)Math.Pow(angle, 2 * i - 2) / factorial(2 * i - 2);
+            }
+            return x;
+        }
+
+        private double sin(double agle, int n)
+        {
+            double x = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                x += Math.Pow(-1, i - 1) * (double)Math.Pow(angle, 2 * i - 1) / factorial(2 * i - 1);
+            }
+            return x;
+        }
+
+        private int factorial(int n)
+        {
+            if (n <= 1) return 1;
+            return n * factorial(n - 1);
+        }
+    }
+}
